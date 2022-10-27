ef1d16cfd4d953fc52891113587270c6ebac9dac
issei <isseieikisouya@outlook.jp>
2022-10-28 02:44:10 +0900

    Q 36;33

--- index.js
+++ index.js
@@ -1,38 +1,11 @@
-const element = <h1 className="header">This is JSX</h1>
-console.log(element)
-
-/* {
-    $$typeof
-: 
-Symbol(react.element)
-key
-: 
-null
-props
-: 
-{className: 'header', children: 'This is JSX'}
-ref
-: 
-null
-type
-: 
-"h1"
-_owner
-: 
-null
-_store
-: 
-{validated: false}
-_self
-: 
-null
-_source
-: 
-null
-}
- */
+const page = (
+    <div>
+        <h1 className = "header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
 
 ReactDOM.render(
-   element,
+    page,
     document.getElementById("root")
 )
