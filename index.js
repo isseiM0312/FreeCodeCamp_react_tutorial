69da147c01037d55893b48cef98a192627ec4ee4
issei <isseieikisouya@outlook.jp>
2022-10-28 02:34:03 +0900

    Q 20:38

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-ReactDOM.render(
-    <ul>
-        <li>1</li>
-        <li>2</li>
-    </ul>,
-    document.getElementById("root")
-)
+const h1 = document.createElement("h1")
+h1.textContent = "This is an imperative way to program"
+h1.className = "header"
+document.getElementById("root").append(h1)
